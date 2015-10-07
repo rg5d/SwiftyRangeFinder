a99f465f9923502b62c5f3fab4a446c372541d76
rg <github@rg4d.me>
2015-10-07 14:57:20 -0700

    added Singleton.swift

---
+++ SwiftyRangeFinder/Singleton.swift
@@ -0,0 +1,15 @@
+//
+//  Singleton.swift
+//  SwiftyRangeFinder
+//
+//  Created by rg on 10/7/15.
+//  Copyright © 2015 RG5D. All rights reserved.
+//
+
+import Foundation
+
+class Singleton: AnyObject {
+
+
+  
+}
